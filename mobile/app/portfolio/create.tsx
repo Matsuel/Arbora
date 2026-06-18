79bc6dfb4d1cd8169c770b7e7d0be4b38689f776
Matsuel <matheolang@icloud.com>
2026-06-18 15:29:41 +0200

    feat: implement portfolio creation component with input and button

--- mobile/app/portfolio/create.tsx
+++ mobile/app/portfolio/create.tsx
@@ -1,12 +1,49 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import Button from '@/components/ui/Button'
+import Input from '@/components/ui/Input'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-const create = () => {
+const Create = () => {
+
+    const [name, setName] = useState<string>('')
+    
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Portfolio Creation</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Création d&apos;un portefeuille
+            </Text>
+
+            <View style={styles.form}>
+                <Input
+                    label="Nom du portefeuille"
+                    value={name}
+                    onChangeText={setName}
+                />
+                <Button disabled={name.length === 0} onPress={() => {}}>
+                    Créer le portefeuille
+                </Button>
+            </View>
         </View>
     )
 }
 
-export default create
+export default Create
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        paddingTop: 50,
+        paddingHorizontal: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    form: {
+        width: '100%',
+        marginTop: 20,
+        gap: 30,
+    },
+});
